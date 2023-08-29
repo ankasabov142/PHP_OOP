88988640754530a8c594a0acae4748d61e7f2803
Angel Kasabov <ankasabov142@gmail.com>
2023-08-30 01:25:27 +0300

    Updated php Functionality

--- createUser.php
+++ createUser.php
@@ -9,9 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $email = $_POST['email'];
     $role = $_POST['role'];
-
     $user = new User(null, $username, $email, $role);
-
 $createdUserId = $userManagement->create($user->toArray());
     if ($createdUserId) {
         echo "User created successfully with ID: $createdUserId";
